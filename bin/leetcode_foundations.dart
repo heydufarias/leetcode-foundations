7e7bb9960695f992be3c7963be5dea1ad72bcf07
dufarias <heydufarias@gmail.com>
2026-04-21 12:41:25 -0300

    feat: Create a dart project

---
+++ bin/leetcode_foundations.dart
@@ -0,0 +1,5 @@
+import 'package:leetcode_foundations/leetcode_foundations.dart' as leetcode_foundations;
+
+void main(List<String> arguments) {
+  print('Hello world: ${leetcode_foundations.calculate()}!');
+}
---
+++ test/leetcode_foundations_test.dart
@@ -0,0 +1,8 @@
+import 'package:leetcode_foundations/leetcode_foundations.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
